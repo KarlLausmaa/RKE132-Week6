5b36918aba9acf3ee0ba5d44550bc448ef25b595
Karl Lausmaa <karl.lausmaa@gmail.com>
2023-04-11 16:18:32 +0300

    Week6

---
+++ Week6/Program.cs
@@ -0,0 +1,31 @@
+﻿string hello = "I am a robot ";
+
+int stringLenght = hello.Length;
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+Console.WriteLine(firstLetter);
+
+
+
+
+
+//int wordCounter = 0;
+
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    //Console.WriteLine($"{i} symbol in string: {hello[i]}");
+//    if (char.IsWhiteSpace(hello[i]))//(hello[i] == ' ')
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine(hello.Length);
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter +1} words");
